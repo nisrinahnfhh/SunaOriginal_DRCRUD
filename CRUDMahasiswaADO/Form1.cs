1f0eebc0e1550e713b3c50666f217454142dc891
Nisrina Hanifah Ramadhani <nisrinahnfhh@gmail.com>
2026-04-09 14:28:47 +0700

    Membuat Variabel Koneksi

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -13,6 +13,9 @@ namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=LAPTOP-1SH0I1SH\\NANA;Initial Catalog=DBAkademikADO;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
